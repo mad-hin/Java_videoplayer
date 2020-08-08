a080c56e7b89ca0adb3525ca132df289a8cef89b
mad-hin <46859133+mad-hin@users.noreply.github.com>
2020-08-08 22:26:18 +0800

    update 08/08/2020

--- src/com/marco/player/videoplayer.java
+++ src/com/marco/player/videoplayer.java
@@ -1,6 +1,8 @@
 package com.marco.player;
 
 import com.marco.player.ui.UI;
+import com.sun.jna.NativeLibrary;
+import uk.co.caprica.vlcj.binding.RuntimeUtil;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -15,15 +17,27 @@ public class videoplayer implements ActionListener {
     UI ui = new UI(this);
 
     public videoplayer() {
-        mainFrame.setSize(600, 400);
+        // set frame to be maximized
+        mainFrame.setExtendedState(mainFrame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+        // set close to stop programme
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // make it resizable
         mainFrame.setResizable(true);
+        // Initialize the Menu
         ui.InitMenu();
+        // Make the frame visible
         mainFrame.setVisible(true);
     }
 
     public static void main(String[] args) {
         new videoplayer();
+        try {
+            NativeLibrary.addSearchPath(RuntimeUtil.getLibVlcLibraryName(), "C:\\Program Files\\VideoLAN\\VLC");
+        } catch (Error e) {
+            System.out.println("Error in Line 32");
+            //noinspection ThrowablePrintedToSystemOut
+            System.out.println(e);
+        }
         try {
             // set the file explore in window like (although I don't think it is look like windows' one)
             UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
